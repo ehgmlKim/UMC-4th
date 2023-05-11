3d74721417e6dab8f1dd5024ae8f7c7005002768
ehgmlKim <dohee930@gmail.com>
2023-05-11 15:01:53 +0900

    Initial commit

---
+++ src/main/java/com/example/umcserver/UmcServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.umcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmcServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/umcserver/UmcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
