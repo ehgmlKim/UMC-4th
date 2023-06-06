2a215876b4bb20435927ad9566ac7b02abfe3f56
ehgmlKim <dohee930@gmail.com>
2023-06-07 02:17:15 +0900

    Add: jsp 파일 생성 및 구현 성공

--- src/main/java/com/example/umc4/testController.java
+++ src/main/java/com/example/umc4/testController.java
@@ -1,12 +1,13 @@
 package com.example.umc4;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class testController {
     @GetMapping("/test")
     public String test(){
-        return "hello";
+        return "index";
     }
 }
